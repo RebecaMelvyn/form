868156540c5704c57349fd0db28d3bde80fc1987
Melvyn <melvyn.rebeca1@gmail.com>
2021-11-17 15:34:47 +0100

    blalbl

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+var nom = document.getElementById('name');
+var mail = document.getElementById('mail');
+var phone = document.getElementById('phone');
+var msg = document.getElementById('msg');
+var sub = document.getElementById('submit');
+
+console.log(nom);
+console.log(mail);
+console.log(phone);
+
+function form() {
+    if (nom.value.length < 3) {
+        alert('toto');
+    } else {
+        return ("le nom est ok");
+    }
+
+    if (phone.value === String) {
+        alert
+    }
+}
+
+sub.addEventListener("click", form);
+
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form JS</title>
+    <link rel="stylesheet" href="assets/styles.css">
+
+</head>
+
+<body>
+
+    <form name="form">
+        <h1>Melvyn</h1>
+        <div>
+            <label for="name">Nom :</label>
+            <input type="text" id="name" name="user_name">
+        </div>
+        <div>
+            <label for="mail">e-mail :</label>
+            <input type="email" id="mail" name="user_mail">
+        </div>
+        <div>
+            <label for="number">Phone :</label>
+            <textarea type="number" id="phone" name="user_phone"></textarea>
+        </div>
+        <div>
+            <label for="msg">Message :</label>
+            <textarea id="msg" name="user_message"></textarea>
+        </div>
+        <div>
+            <input id="submit" type="submit">
+        </div>
+
+    </form>
+    <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
